b0317114e73833eb808a282d465f2f86cdf45447
Nils Wloka <nils.wloka@codecentric.de>
2016-09-21 09:52:01 +0200

    First pie chart

--- src/App.js
+++ src/App.js
@@ -4,27 +4,35 @@ const { Surface, Group, Shape } = ART;
 
 import * as scale from 'd3-scale';
 import * as shape from 'd3-shape';
+import * as core from 'd3';
 import * as d3Array from 'd3-array';
 const d3 = {
   scale,
   shape,
+  core
 };
 
 class App extends Component {
   render() {
-    var arc = d3.shape.arc()
-      .innerRadius(0)
-      .outerRadius(100)
-      .startAngle(0)
-      .endAngle(Math.PI / 2)();
+    const data = [4, 8, 15, 16, 23, 42];
+    const colors = ['#ffffff', '#ff0000', '#00ff00', '#0000ff', '#ff00ff', '#00ffff'];
+    const pie = d3.shape.pie()(data);
+    const arcs = pie.map(function (arc) {
+      return d3.shape.arc()(Object.assign({}, arc, {outerRadius: 100, innerRadius: 50}));
+    });
+    const shapes = arcs.map(function (arc, i) {
+      return <Shape
+        key={i}
+        d={arc}
+        stroke="#000"
+        fill={colors[i]}
+        strokeWidth={1} />
+    });
     return (
-      <View>
+      <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
         <Surface width={500} height={500}>
-          <Group x={100} y={200}>
-            <Shape
-              d={arc}
-              stroke="#000"
-              strokeWidth={1} />
+          <Group x={250} y={250}>
+            {shapes}
           </Group>
         </Surface>
       </View>
