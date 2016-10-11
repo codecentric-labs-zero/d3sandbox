c886761881205a4c1ddc24aea042a76ad68fb09e
Nils Wloka <nils.wloka@codecentric.de>
2016-10-11 12:24:07 +0200

    Added comments

--- src/App.js
+++ src/App.js
@@ -99,20 +99,26 @@ class PieChart extends Component {
 class ColorChangingBackground extends Component {
   constructor(props) {
     super(props)
-    this.state = {
-      currentColor: 'rgb(102, 102, 102)',
-      nextColor: 'rgb(102, 102, 102)'
-    }
-    this._colorValue = new Animated.Value(0)
-    this._flash = this._flash.bind(this)
-    this._fadeOver = this._fadeOver.bind(this)
-    this._flashingAnimation = null
+    // Some color definitions
     this.ORANGE = 'rgb(255, 153, 0)'
     this.GREY = 'rgb(102, 102, 102)'
     this.RED = 'rgb(192,33,8)'
+    // Initialize everything to grey when mounting the component
+    this.state = {
+      currentColor: this.GREY,
+      nextColor: this.GREY
+    }
+    // We'll almost always go from 0 to 100, transitioning from current to next color,
+    // so initialize _colorValue to 0
+    this._colorValue = new Animated.Value(0)
+    this._flash = this._flash.bind(this)
+    this._fadeOver = this._fadeOver.bind(this)
   }
 
   componentWillReceiveProps(nextProps) {
+    // Determine next color based on next intensity
+    // Please note that this implementation assumes that intensity
+    // can only be adjusted in steps of 1
     var nextColor
     switch (nextProps.intensity) {
       case 0:
@@ -125,34 +131,30 @@ class ColorChangingBackground extends Component {
         nextColor = this.RED
         break
     }
+    // Only trigger animation when something changes
     if (this.props.intensity !== nextProps.intensity) {
+      // Set target color
       this.setState({nextColor: nextColor})
+      // We need to do some more things when the flashing animation is running
       if (this.props.intensity === 2) {
+        // Stop flash animation and grab current value
         this._colorValue.stopAnimation((value) => {
+          // Create new starting point for fading back to orange (remember
+          // that we always fade from 0 - 100
           this._colorValue = new Animated.Value(100 - value)
+          // Make sure that we fade from red to orange
           this.setState({currentColor: this.RED, nextColor: this.ORANGE})
+          // And trigger normal fade
           this._fadeOver(nextProps, nextColor);
         })
       } else {
+        // Reset to 0
         this._colorValue = new Animated.Value(0)
+        // And trigger normal fade
         this._fadeOver(nextProps, nextColor);
       }
 
     }
-  }
-
-  _fadeOver(nextProps, nextColor) {
-    Animated.timing(this._colorValue, {
-      toValue: 100,
-      duration: 1000
-    }).start((event) => {
-      if (nextProps.intensity !== 2) {
-        this.setState({currentColor: nextColor})
-      } else {
-        this.setState({currentColor: this.ORANGE, nextColor: this.RED})
-        this._flash()
-      }
-    })
   }
 
   render() {
@@ -178,17 +180,40 @@ class ColorChangingBackground extends Component {
     )
   }
 
+  _fadeOver(nextProps, nextColor) {
+    Animated.timing(this._colorValue, {
+      toValue: 100,
+      duration: 1000
+    }).start((event) => {
+      // When the animation finishes, we need to do some cleanup
+      if (nextProps.intensity !== 2) {
+        // For "normal" intensities, just make sure to update current color
+        this.setState({currentColor: nextColor})
+      } else {
+        // For "flashing" intensity, we want to start the flashing effect after
+        // we have faded to red
+        this.setState({currentColor: this.ORANGE, nextColor: this.RED})
+        this._flash()
+      }
+    })
+  }
+
   _flash() {
+    // This is just a sequence going from 100 to 0 and back.
+    // As we always start _flash after a normal transition, colorValue will
+    // already be 100, so start going to 0
     Animated.sequence([
       Animated.timing(this._colorValue, {
-        toValue: 100,
+        toValue: 0,
         duration: 500,
       }),
       Animated.timing(this._colorValue, {
-        toValue: 0,
+        toValue: 100,
         duration: 500
       })
     ]).start((event) => {
+      // Only restart loop when we finished naturally. Otherwise
+      // stopAnimation will not be able to interrupt the loop
       if (event.finished) {
         this._flash();
       }
